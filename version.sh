648dfbbc829e57687c2bd074fe6de7716c8ec3ab
stephent <>
2002-05-09 12:03:13 +0000

    [xemacs-21-4-hg @ 2002-05-09 12:02:24 by stephent] XEmacs 21.4.8 "Honest Recruiter" is released.

--- version.sh
+++ version.sh
@@ -2,8 +2,8 @@
 emacs_is_beta=
 emacs_major_version=21
 emacs_minor_version=4
-emacs_beta_version=7
-emacs_kit_version=
+emacs_beta_version=8
+xemacs_codename="Honest Recruiter"
 xemacs_codename="Economic Science"
 infodock_major_version=4
 infodock_minor_version=0
