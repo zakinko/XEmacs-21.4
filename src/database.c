75c55a17f0938be5e2cc50e4e403a0f99ac4de2e
vins <>
2003-03-05 03:28:40 +0000

    [xemacs-21-4-hg @ 2003-03-05 03:28:28 by vins] More fixes for 21.4.13 from Martin, Stephen, Ben, etc.

--- src/database.c
+++ src/database.c
@@ -45,11 +45,13 @@ Boston, MA 02111-1307, USA.  */
 #ifdef HAVE_INTTYPES_H
 #define __BIT_TYPES_DEFINED__
 #include <inttypes.h>
+#ifndef __FreeBSD__
 typedef uint8_t  u_int8_t;
 typedef uint16_t u_int16_t;
 typedef uint32_t u_int32_t;
 #ifdef WE_DONT_NEED_QUADS
 typedef uint64_t u_int64_t;
+#endif
 #endif /* WE_DONT_NEED_QUADS */
 #endif /* HAVE_INTTYPES_H */
 #endif /* !(defined __GLIBC__ && __GLIBC_MINOR__ >= 1) */
