f73dfc90800011cfe4867aa95dcb111538bc514f
vins <>
2003-10-21 00:22:11 +0000

    [xemacs-21-4-hg @ 2003-10-21 00:22:05 by vins] Next set of patches for 21.4.15

--- src/regex.c
+++ src/regex.c
@@ -1135,7 +1135,7 @@ static const char *re_error_msgid[] =
    exactly that if always used MAX_FAILURE_SPACE each time we failed.
    This is a variable only so users of regex can assign to it; we never
    change it ourselves.  */
-#if defined (MATCH_MAY_ALLOCATE)
+#if defined (MATCH_MAY_ALLOCATE) || defined (REGEX_MALLOC)
 /* 4400 was enough to cause a crash on Alpha OSF/1,
    whose default stack limit is 2mb.  */
 int re_max_failures = 20000;
